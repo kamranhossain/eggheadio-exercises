ed0d1bc3040ed8cebad41bbee2754c580f72d1de
bleedingpoem <kamran.hossain227@gmail.com>
2017-12-10 06:34:14 +0600

    pass-values-into-angular-2-components-with-input

---
+++ get-start-with-angular/8/angular-fundamentals/src/app/simple-form/simple-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-form',
+  template: `<div>
+  {{message}}
+  <input #myInput type="text">
+  <button (click)="onClick($event, myInput.value)">Click me</button>
+
+  </div>`,
+  styles: []
+})
+export class SimpleFormComponent implements OnInit {
+
+  @Input() message;
+
+  onClick(event, value) {
+    console.log(event)
+    console.log(value)
+  }
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
